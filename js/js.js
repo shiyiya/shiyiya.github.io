3892ee0fdef1e80a728eb1dd8f949ae6a1c01e9d
shiyiya <shiyiya@users.noreply.github.com>
2023-09-08 13:25:51 +0000

    deploy: cae786ffa8e1d0c247467ae890349d2ca4f2a2be

--- js/js.js
+++ js/js.js
@@ -28,7 +28,7 @@ if ($comments) {
     }
   })
 
-  fetch(`https://oaii.vercel.app/api/comment?post_id=${document.body.getAttribute('path')}`).then(comments => {
+  fetch(`https://oaii.vercel.app/api/comment?post_id=${document.body.getAttribute('path')}`).then(it => it.json()).then(comments => {
     document.querySelector('.vempty').insertAdjacentHTML('beforebegin', comments.map(comment => {
       return `<li>${comment.username}: ${comment.content} <small style='color:#bbb'>${new Date(comment.createdAt).toLocaleString()}</small></li>`
     }))
